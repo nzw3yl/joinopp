0cde5577dbd02864ed20a9b33c54596c4b13e4ca
Roger Smith <roger.smith@ceryx.com>
2011-03-25 22:12:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joinopp::Application.config.secret_token = 'b504d441a94f86c927ac64c7d794c0c49d61084c28cc0a70c7d4403ff49fa178415142c96150317f41377b33b787fdfb003db934f33fabe86cad96cf6f361f59'
