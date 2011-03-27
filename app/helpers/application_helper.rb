42074f34e3d3ac0bcc2663053eb8c814fb0df030
Roger Smith <roger.smith@ceryx.com>
2011-03-26 22:29:29 -0400

    updated layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+   # Return a title ona per-page basis
+   def title
+      base_title = "JoinOpp"
+      if @title.nil?
+	base_title
+      else
+        "#{base_title} | #{@title}"
+      end
+   end
 end
