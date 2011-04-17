b6de86ed09c09bc80e128821fa91a7431eeaa140
Roger Smith <roger.smith@ceryx.com>
2011-04-17 01:40:56 -0400

    fixing status_id casting error on heroku2

--- db/migrate/20110416190909_change_status_id_to_integer_in_user.rb
+++
@@ -1,9 +0,0 @@
-class ChangeStatusIdToIntegerInUser < ActiveRecord::Migration
-  def self.up
-     change_column :users, :status_id, :integer
-  end
-
-  def self.down
-     change_column :users, :status_id, :string
-  end
-end
